0830051756fde6794b8b7442acf9ec3b53b8b076
sahana123xworkz@gmail.com <sahana123xworkz@gmail.com>
2025-11-13 18:56:06 +0530

    functions

---
+++ arrow.js
@@ -0,0 +1,18 @@
+//arrow function
+let p=()=>{
+console.log("Gm")
+
+}
+console.log(p);
+p();
+
+
+
+var funcname=(p,q)=>{
+console.log("Hi,Hello");
+console.log(p,q);
+return "yui";
+}
+console.log(funcname)
+console.log(funcname())
+console.log(funcname("HYD","IND"));
---
+++ script.js
@@ -0,0 +1,35 @@
+//let 
+let name="Nandu";
+function greet(){
+    name="JK";
+    let x=56;
+     x=678;
+console.log(name);
+console.log(x)
+}
+console.log(name);
+greet();
+
+//var
+var price=77;
+var price=234;
+function b(){
+    price=34;
+    console.log(price);
+}
+b();
+
+//const
+
+const total=777;
+function a(){
+    if(4567){
+        value="JavaScript";
+        console.log(total);
+        console.log(value)
+        
+    }
+    console.log(value);    
+}
+
+a();
