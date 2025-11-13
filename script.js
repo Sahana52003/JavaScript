e57ea3e65d1cbd4ac3dd4799869b8f522c478420
sahana123xworkz@gmail.com <sahana123xworkz@gmail.com>
2025-11-13 18:35:46 +0530

    Functions

---
+++ script.js
@@ -0,0 +1,63 @@
+console.log("I'm Sahana N from Kolar");
+console.log("I recently graduated by BE in ISE from SSIT ,Tumkr");
+console.log("Through my academic I had gained skills like java,js and the web technologies like HTML,CSS");
+console.log("Backto my 10 and PU ,I had completed my studies in Sri Byraveshwara Rural Composite Pu College");
+console.log("And then I had done two projects in my College : 1.Screen time analysis and 2. Epileptic Seizure detection in EEG signals");
+console.log("In EEG project,It will detect whether a person having a seizure or not,Where the process it will collect a raw data then it will remove a noise/filter unnecessary data and then processing the model,after filter the data ,I had took two ML model like Random Forest Classifier and Support Vector Machine>By Using this I can detect a person ");
+console.log("And then I had took two certificates like HTML/CSS and Web development through this I had build a mini project");
+console.log("Then my hobbies are watching movies,playing and listing songs");
+console.log("Languages:Kannada,English,Telugu");
+
+
+//primitive and non-primitive 
+var num=77;
+console.log(num);
+var name="XYS";
+name="JK";
+console.log(name);
+var x;
+console.log(x);
+var value=23.343;
+console.log(value);
+console.log(typeof(value));
+
+var p='$';
+console.log(typeof p);
+
+var y=Symbol("PQR");
+console.log(typeof y);
+
+var n=2345n;
+console.log(typeof n);
+
+var p=17;
+console.log(typeof p);
+var q=false;
+console.log(typeof q);
+
+var arr=[2,7,1,"sora",false];
+console.log(arr);
+console.log(typeof arr);
+
+var dict={age:45,name:"Nandu"};
+console.log(dict);
+console.log(typeof(dict));
+
+
+
+//function
+function primeNumber(){
+
+    console.log("Prime number is");
+
+}
+console.log("Prime Number huok")
+primeNumber();
+console.log("rtyuiop[");
+console.log(typeof primeNumber);
+
+
+
+//hoist
+console.log("Hello,Gm");
+var nam="uiop";
---
+++ scripts.js
@@ -0,0 +1,154 @@
+console.log("=======================");
+var num=34567;
+console.log(typeof num);
+
+var num1=23453421n;
+console.log(typeof num1);
+
+var name="NAndu";
+console.log(typeof name);
+
+var value=3456.3427;
+console.log(typeof value);
+
+var isAvaliable=true;
+console.log(typeof isAvaliable);
+
+var x=null;
+console.log(typeof x);
+
+var price;
+console.log(typeof price);
+
+var total=Symbol(34567);
+console.log(typeof total);
+
+var arr=[2.2,5455,"Ram",false];
+console.log(typeof arr);
+for(var i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+
+var obj={name:"Jk",age:29}
+console.log(typeof obj);
+console.log(obj.age);
+
+function greet(){
+ console.log("Hello");
+}
+greet();
+
+
+
+
+
+console.log("=============================");
+console.log("Hello Good Moring");
+var nam="Ram";
+
+console.log("number",number);
+var number=567;
+
+console.log(arr1);
+var arr1=[2.2,5455,"Ram",false];
+
+console.log(obj1);
+var obj1={name:"Jk",age:29};
+
+console.log(num2);
+var num2=23453421n;
+
+console.log(name1);
+var name1="NAndu";
+
+console.log(value1);
+var value1=3456.3427;
+
+console.log(isAvaliabl);
+var isAvaliabl=true;
+
+console.log(xy);
+var xy=null;
+
+console.log(price1)
+var price1;
+
+console.log(total1);
+var total1=Symbol(34567);
+
+
+console.log("=======================");
+var data=10; 
+data=20;     
+console.log(data);  
+
+data="Hello";
+data="GM";      
+console.log(data);  
+
+data=true;  
+data=false;       
+console.log(data);  
+
+data={name: "Sora"};
+data={name:"pooja"};
+console.log(data); 
+
+
+
+//let
+let surname="Sahana";
+surname='Sora';
+console.log(surname);
+
+//console.log(namber);
+//let namber=34;
+
+
+//const
+
+const values=45;
+console.log(values);
+
+
+// console.log(prices);
+// const prices=84;
+
+
+//function
+
+var amt=3400;
+function myVar(){
+    console.log(amt);
+}
+var amt=56;
+myVar();
+
+
+let detamt=4567;
+function main(){
+    detamt=2354;
+    console.log(detamt);
+}
+console.log(detamt);
+main();
+
+
+
+const _price=function a(){
+    console.log("Hello");
+}
+console.log(_price);
+console.log(_price());
+
+
+//arrow function
+var s=()=>{
+console.log("Universe");
+
+}
+console.log(s);
+console.log(s());
+
+
+var s=()=>false;
